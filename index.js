c5bbc2400129ddc1d894487348ba922bde47b0c8
WALTER DAVID IRUNGU <daveirungu20@gmail.com>
2024-07-04 20:40:45 +0300

    LOOPING THRUOGH ARRAYS

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+let arrayz=['Guadalupe','Ollie','Aki']
+function writeCards(arrayNames,event_name){
+    let array_of_names=[];
+    for(let i=0;i<arrayNames.length;i++){
+    
+     array_of_names.push(`Thank you, ${arrayNames[i]}, for the wonderful ${event_name} gift!`);
+    }
+    return array_of_names;
+}
+console.log(writeCards(arrayz,"suprise"));
+
+function countDown(integer){
+    while(integer>=0){
+        console.log(integer);
+        integer--
+    }
+    
+}
+console.log(countDown(7));
